e2084f0fcbfbc09061cab1aa3d682757a13d220b
Bill Evans <billev2k@gmail.com>
2021-02-16 10:02:14 -0800

    New user pool, canonicalize email casing, report database repositories.
    - Implemented support for the new Cognito user pool, with a message returned
      when someone signed in through the old user pool.
    - Now properly treat "Me@example.org" the same as "me@example.org".
    - Programs may now be configured with a "repository" property, which is
      returned along with the programs and the user's roles in those programs.

--- cognitoTriggerHandler/cognitoTriggerHandler/lambda_function.py
+++ cognitoTriggerHandler/cognitoTriggerHandler/lambda_function.py
@@ -1,17 +1,25 @@
 import hashlib
+from datetime import datetime
+from typing import List, Dict
 
 import boto3
 from amplio.rolemanager import manager
 
 REGION_NAME = 'us-west-2'
 
-TABLE_NAME = 'acm_users'
+USERS_TABLE_NAME = 'acm_users'
+PROGRAMS_TABLE_NAME = 'programs'
 dynamodb = boto3.resource('dynamodb', region_name=REGION_NAME)
-table = dynamodb.Table(TABLE_NAME)
+users_table = dynamodb.Table(USERS_TABLE_NAME)
+programs_table = dynamodb.Table(PROGRAMS_TABLE_NAME)
+
 USER_POOL_ID = 'us-west-2_6EKGzq75p'
 cognito_client = boto3.client('cognito-idp')
 
 manager.open_tables()
+
+WARNING_DATE = None
+OLD_POOL_ID = 'us-west-2_6EKGzq75p'
 
 def _email_from_event(event: dict) -> str:
     """
@@ -20,7 +28,7 @@ def _email_from_event(event: dict) -> str:
     try:
         request = event['request']
         user_attributes = request['userAttributes']
-        email = user_attributes['email']
+        email = user_attributes['email'].lower()
     except Exception as e:
         print('Exception retrieving email:  ' + str(e))
         print('Event: ' + str(event))
@@ -28,14 +36,31 @@ def _email_from_event(event: dict) -> str:
 
     return email
 
+def get_configured_repositories(programs: List[str], default: str = 'dbx', repositories=['s3']) -> Dict[str,List[str]]:
+    """
+    Given a list of programs, find the ones that do not use the default repository.
+    """
+    default = default.lower()
+    if default != 'dbx':
+        print('Warning! get_configured_repositories called with non-standard default repository: {}'.format(default))
+    result = {}
+    program_items = programs_table.scan()['Items']
+    for item in program_items:
+        program = item.get('program')
+        repository = item.get('repository', default).lower()
+        if program in programs and repository in repositories:
+            list = result.setdefault(repository, [])
+            list.append(program)
+    return result
 
-def get_user_access(email):
+
+def get_user_access(email, user_pool_id=None):
     """
     Given a user's email address, return their access as given in the acm_users table.
     Note that the access may be given to all users at the domain.
     """
     # First look up by user's full email address.
-    acm_user = table.get_item(Key={'email': email})
+    acm_user = users_table.get_item(Key={'email': email})
     user_info = acm_user.get('Item')
     if not user_info:
         # Nothing specific to the user; look up by email domain.
@@ -44,14 +69,20 @@ def get_user_access(email):
             # This should never happen, because a user needs a valid email address to sign up.
             raise Exception("'{}' is not a valid email address.".format(email))
         org = parts[1]
-        acm_user = table.get_item(Key={'email': org})
+        acm_user = users_table.get_item(Key={'email': org})
         user_info = acm_user.get('Item')
         # If the user's organization isn't in the table, give them no access at all.
         if not user_info:
             user_info = {'edit': '', 'view': '', 'admin': False}
 
     # program:roles;program:roles...
-    programs = ';'.join([p+':'+r for p,r in manager.get_programs_for_user(email).items()])
+    #programs = ';'.join([p+':'+r for p,r in manager.get_programs_for_user(email).items()])
+    programs_for_user = manager.get_programs_for_user(email)
+    # "s3:TEST;dbx:DEMO,XTEST-2"
+    configured_repositories = ';'.join(f'{k}:{",".join(v)}' for k,v in get_configured_repositories(list(programs_for_user.keys())).items())
+    print('Repositories: {}'.format(configured_repositories))
+    programs = ';'.join([p + ':' + r for p, r in programs_for_user.items()])
+
     try:
         md5 = hashlib.md5()
         md5.update(email.lower().encode('utf-8'))
@@ -63,8 +94,22 @@ def get_user_access(email):
                    'admin': user_info.get('admin', False),
                    'programs': programs
                    }
+    if configured_repositories:
+        user_access['repositories'] = configured_repositories
     if hash:
         user_access['hash'] = hash
+    # Warning to create new ID? Use Message-Of-Day facility.
+    if user_pool_id == OLD_POOL_ID:
+        global WARNING_DATE
+        if not WARNING_DATE:
+            WARNING_DATE = datetime(2021,1,25,0,0,0,0)
+        if datetime.now() > WARNING_DATE:
+            user_access['mod'] = 'Your sign-in was successful, and you may proceed.\n'+\
+                                 'However, due to necessary software maintenance, you\n'+\
+                                 'need to create a new Amplio ID by 2021-February-1.\n\n'+\
+                                 'Contact support@amplio.org if you need assistance.'
+            user_access['modButton'] = "Proceed"
+
     print('Access for user {}: {}'.format(email, user_access))
     return user_access
 
@@ -121,8 +166,9 @@ def token_generation_handler(event):
     if "response" not in event:
         event["response"] = {}
     email = _email_from_event(event)
+    user_pool_id = event.get('userPoolId')
     event["response"]["claimsOverrideDetails"] = {
-        "claimsToAddOrOverride": get_user_access(email)
+        "claimsToAddOrOverride": get_user_access(email, user_pool_id)
     }
 
 
@@ -130,7 +176,7 @@ def token_generation_handler(event):
 def lambda_handler(event, context):
     print(event)
     trigger = event.get('triggerSource', '')
-    if trigger == 'TokenGeneration_Authentication' or trigger == 'TokenGeneration_RefreshTokens':
+    if trigger == 'TokenGeneration_Authentication' or trigger == 'TokenGeneration_RefreshTokens' or trigger.startswith('TokenGeneration'):
         token_generation_handler(event)
     elif trigger == 'PreSignUp_SignUp':
         pre_signup_handler(event)
@@ -194,6 +240,28 @@ if __name__ == '__main__':
         print(access)
         rc = 0 if access['view'] == '.*' and access['admin'] == 'false' else 1
         rc_list.append(rc)
+
+        programs = ['TEST', 'XTEST-2', 'DEMO']
+        repos = ['s3', 'dbx']
+        configured = get_configured_repositories(programs, repositories=repos)
+        print("Configured '{}': {}".format(repos, configured))
+        repos = ['s3']
+        configured = get_configured_repositories(programs, repositories=repos)
+        print("Configured '{}': {}".format(repos, configured))
+        repos = ['dbx']
+        configured = get_configured_repositories(programs, repositories=repos)
+        print("Configured '{}': {}".format(repos, configured))
+        repos = None
+        configured = get_configured_repositories(programs)
+        print("Configured '{}': {}".format(repos, configured))
+
+        configured = get_configured_repositories(programs, 's3')
+        print("Configured '{}': {}", 's3', configured)
+        configured = get_configured_repositories(programs, 'dbx')
+        print("Configured '{}': {}", 'dbx', configured)
+
+        access = get_access('bill@amplio.org')
+        print(access)
 
         return max(rc_list)
 
